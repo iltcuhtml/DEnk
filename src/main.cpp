eae63825da9154cb19f25224fea1a01560c94e8a
10119-JYC-Mario-O-O <toadchen83@gmail.com>
2025-06-07 22:29:54 +0900

    main update

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,9 @@
 
 #include <sstream>
 #include <vector>
-
 #include <optional>
-
 #include <fstream>
+#include <filesystem>
 
 #include "tokenizer.hpp"
 #include "parser.hpp"
@@ -22,22 +21,21 @@ int main(int argc, char* argv[])
         return EXIT_FAILURE;
     }
 
-    std::string contents;
+    const std::string filename = argv[1];
 
+    std::ifstream input(filename, std::ios::binary);
+    
+    if (!input)
     {
-        std::ifstream input(argv[1], std::ios::binary);
-
-        if (!input.is_open())
-        {
-            std::cerr << "Fehler: Die Datei konnte nicht geöffnet werden" << std::endl;
-            return EXIT_FAILURE;
-        }
-
-        std::ostringstream contents_stream;
-        contents_stream << input.rdbuf();
+        std::cerr << "Fehler: Die Datei konnte nicht geöffnet werden" << std::endl;
         
-        contents = contents_stream.str();
+        return EXIT_FAILURE;
     }
+
+    std::string contents(
+        (std::istreambuf_iterator<char>(input)),
+        std::istreambuf_iterator<char>()
+    );
 
     Tokenizer tokenizer(std::move(contents));
     std::vector<Token> tokens = tokenizer.tokenize();
@@ -52,24 +50,45 @@ int main(int argc, char* argv[])
         return EXIT_FAILURE;
     }
 
+    std::error_code ec;
+
+    if (!std::filesystem::exists("out") && !std::filesystem::create_directory("out", ec))
     {
-        Generator generator(prog.value());
+        std::cerr << "Fehler: Ausgabeverzeichnis konnte nicht erstellt werden: " << ec.message() << std::endl;
         
-        system("mkdir out > nul");
-
-        std::ofstream file("out/out.asm", std::ios::out | std::ios::binary);
-
-        if (!file)
-        {
-            std::cerr << "Fehler: Die Ausgabedatei konnte nicht erstellt werden" << std::endl;
-            return EXIT_FAILURE;
-        }
-
-        file << generator.gen_prog();
+        return EXIT_FAILURE;
     }
 
-    system("nasm -f win64 out/out.asm -o out/out.o");
-    system("gcc out/out.o -o out/out");
+    Generator generator(prog.value());
+
+    std::ofstream file("out/out.asm", std::ios::out | std::ios::binary);
+    
+    if (!file)
+    {
+        std::cerr << "Fehler: Die Ausgabedatei konnte nicht erstellt werden" << std::endl;
+        
+        return EXIT_FAILURE;
+    }
+
+    file << generator.gen_prog();
+
+    int ret = system("nasm -f win64 out/out.asm -o out/out.o");
+    
+    if (ret != 0)
+    {
+        std::cerr << "Fehler: NASM-Assembler konnte nicht erfolgreich ausgeführt werden" << std::endl;
+        
+        return EXIT_FAILURE;
+    }
+
+    ret = system("gcc out/out.o -o out/out");
+    
+    if (ret != 0)
+    {
+        std::cerr << "Fehler: GCC-Linker konnte nicht erfolgreich ausgeführt werden" << std::endl;
+        
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
